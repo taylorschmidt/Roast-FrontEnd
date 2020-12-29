28c9033ff7dc7ed6935b34b36fb517bdcb9086ec
taylorschmidt <taylorhillschmidt@gmail.com>
2020-12-29 14:40:17 -0600

    attempting to connect front and back end on addCafe route

--- src/components/Cafe.js
+++ src/components/Cafe.js
@@ -9,28 +9,37 @@ const Cafe = (params) => {
     const currentUser = getCurrentUser()
     const currentUserId = currentUser.id
     const [yelpId, setYelpId] = useState(cafeData.id)
+    console.log(yelpId)
     const APIURL="https://localhost:8080/api/cafe/all"
 
-    const addToFavorites = (yelpId, currentUserId) => {
-        return axios.post(APIURL, {
-            currentUserId, yelpId
-        })
-        .then((res)=> {
-            console.log(res)
-        })
-        .catch((err)=> {
-            console.log(err)
-        })
-    }
-
+    // const addToFavorites = (YelpId) => {
+    //     return axios.post(APIURL, {
+    //         // id, 
+    //         YelpId
+    //     })
+    //     .then((res)=> {
+    //         console.log(res)
+    //     })
+    //     .catch((err)=> {
+    //         console.log(err)
+    //     })
+    // }
+    console.log(currentUserId)
     const handleClick = (e) => {
         e.preventDefault()
-        if(currentUserId){
-            addToFavorites(yelpId, currentUserId)
-        } else {
-            console.log("you are not logged in!")
-        }
+        
+        axios.post('https://localhost:8080/api/cafe/all', {YelpId: yelpId})
+        .then(res => console.log(res.data))
+        .catch(err => console.log(err))
     }
+    // const handleClick = (e) => {
+    //     e.preventDefault()
+    //     if(currentUserId){
+    //         addToFavorites(yelpId, currentUserId)
+    //     } else {
+    //         console.log("you are not logged in!")
+    //     }
+    // }
     return (
         <div>
             {cafeData.name}
