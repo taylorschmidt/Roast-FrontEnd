a40b280470a0f97b9ce58d73130cebad47d28f41
taylorschmidt <taylorhillschmidt@gmail.com>
2020-12-30 16:53:08 -0600

    added comments form and post route

--- src/components/Cafe.js
+++ src/components/Cafe.js
@@ -1,88 +1,124 @@
-import React, {useState, useEffect} from 'react'
-import {Link} from 'react-router-dom';
-import axios from 'axios'
-import {getCurrentUser} from '../services/auth.service'
+import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { getCurrentUser } from "../services/auth.service";
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
 
 const Cafe = (params) => {
-    console.log(params.location.state.data)
-    const cafeData = params.location.state.data
-    const currentUser = getCurrentUser()
-    const currentUserId = currentUser.id
-    const [yelpId, setYelpId] = useState(cafeData)
-    console.log("LOOK HERE!", yelpId.id)
-    const APIURL="https://localhost:8080/api/cafe/all"
+  const form = useRef()
+  console.log(params.location.state.data);
+  const cafeData = params.location.state.data;
+  const currentUser = getCurrentUser();
+  const currentUserId = currentUser.id;
+  const [yelpId, setYelpId] = useState(cafeData);
+  const [content, setContent] = useState('')
+  
 
+  const handleClick = (e) => {
+    e.preventDefault();
+    const yelp = {
+      id: currentUserId,
+      YelpId: yelpId.id,
+    };
+    axios
+      .post("http://localhost:8080/api/cafe/all", yelp)
+      .then((res) => console.log("favorites:", res))
+      .catch((err) => console.log(err));
+  };
 
+  const addYelpInfo = () => {
+    console.log("Yelp: ", { YelpId: yelpId.id });
+    const yelp = {
+      YelpId: yelpId.id,
+      Name: yelpId.name,
+      Address: yelpId.location.address1,
+      City: yelpId.location.city,
+      Rating: yelpId.rating,
+      ImageURL: yelpId.image_url,
+      YelpURL: yelpId.url,
+    };
+    axios
+      .post("http://localhost:8080/api/yelp", yelp)
+      .then((res) => {
+        console.log("here is our new cafe saved on page load", res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } 
 
-    
-    // const addToFavorites = (YelpId) => {
-    //     return axios.post(APIURL, {
-    //         // id, 
-    //         YelpId
-    //     })
-    //     .then((res)=> {
-    //         console.log(res)
-    //     })
-    //     .catch((err)=> {
-    //         console.log(err)
-    //     })
-    // }
-    console.log(currentUserId)
-    const handleClick = (e) => {
-        e.preventDefault()
-        const yelp = {
-            id: currentUserId,
-            YelpId: yelpId.id
-        }
-        console.log('YELP OBJ:' , yelp)
-        axios.post('http://localhost:8080/api/cafe/all', yelp)
-        .then(res => console.log("favorites:", res))
-        .catch(err => console.log(err))
-    }
-    // const handleClick = (e) => {
-    //     e.preventDefault()
-    //     if(currentUserId){
-    //         addToFavorites(yelpId, currentUserId)
-    //     } else {
-    //         console.log("you are not logged in!")
-    //     }
-    // }
-    useEffect(() => {
-        console.log("Yelp: ", {YelpId: yelpId.id})
-        const yelp = {
-            YelpId: yelpId.id,
-            Name: yelpId.name,
-            Address: yelpId.location.address1,
-       City: yelpId.location.city,
-       Rating: yelpId.rating,
-       ImageURL: yelpId.image_url,
-       YelpURL: yelpId.url
+  const submitComment = (e) => {
+      //addcomment to backend
+      e.preventDefault()
+      form.current.validateAll()
+      addComment()
+  }
 
-        }
-       axios.post("http://localhost:8080/api/yelp", yelp)
-        .then((res)=>{
-            console.log("here is our console.log", res.data)
-    
-        })
-        .catch((err)=>{
-             console.log(err)
-        })
-      },[])
+  const addComment = () => {
+    console.log("Here is the input:", currentUserId, content, yelpId.id)
+    const newComment = {
+        userId: currentUserId,
+        content: content,
+        cafeId: yelpId.id
+      }
+      axios
+      .post("http://localhost:8080/api/comments", newComment)
+      .then((res) => {
+        console.log("here is our new comment:", res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-
-
-
-    return (
-         
-        <div > 
-            {cafeData.name}
-            <button onClick={handleClick}>Add to Favorites</button>
-            
-        </div>
-
-        
-        //write function to save YelpId to backend
-    )
+const displayComments = (pageId) => {
+    axios.get("http://localhost:8080/api/comments", {cafeId: pageId})
+    .then((res) => {
+        console.log("here are all the comments:", res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 }
 
-export default Cafe
+
+
+  const onChangeContent = (e) => {
+    const typedContent = e.target.value
+    setContent(typedContent)
+}
+
+  useEffect(() => {
+    addYelpInfo()
+    displayComments(yelpId.id)
+  }, []);
+  
+
+  return (
+    <>
+      <h1>{yelpId.name}</h1>
+      <h3>{yelpId.location.address1}</h3>
+      <img src={yelpId.image_url} />
+      <button onClick={handleClick}>Add to Favorites!</button>
+      <div>
+        <Form onSubmit={submitComment} ref={form}>
+          <Input
+            type="text"
+            placeholder="add a review"
+            className="form-control"
+            name="comment"
+            value={content}
+            onChange={onChangeContent}
+          />
+        </Form>
+      </div>
+      <div>
+        {/* display all current comments */}
+        Comments Will Populate Here
+      </div>
+    </>
+  );
+};
+
+export default Cafe;
