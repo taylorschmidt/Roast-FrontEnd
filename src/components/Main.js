0ba5653af80a7253e01cbf850cef553c5314126a
taylorschmidt <taylorhillschmidt@gmail.com>
2021-01-05 17:46:10 -0700

    styling main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,15 +3,19 @@ import {Link} from 'react-router-dom'
 
 const Main = () => {
     return (
-<div className="container">
-        Roast
+<div className="container" className="homeContainer">
+        <h1>Roast</h1>
+        <br></br>
         <div className="row">
             <div className="col-sm-9">Find the coziest cafes in your area with a simple search.</div>
             <div className="col-sm-3">PHOTO</div>
         </div>
         <div>
-        <Link to='/home'>Get Started</Link>
-        <button>Get Started</button>
+            <br></br>
+            <br></br>
+        <Link to='/home'>
+        <button className="btn btn-dark">Get Started</button>
+        </Link>
         </div>
 </div>
     )
